e7ff7cbf38558277f59226d287bee661e124bac8
Mahibalan <mahibalan0901@gmail.com>
2024-03-29 15:20:59 +0530

    Exception Exercise

---
+++ Assignments/Exception Excercise/CountLetters.java
@@ -0,0 +1,29 @@
+package mahiapp;
+
+import java.util.Scanner; 
+public class CountLetters{
+	public static void main(String[] args) {
+		int[] counts=new int [26]; 
+		Scanner scan = new Scanner(System.in);
+		//get word from user 
+		System.out.println("Enter a single word (letters only, please): "); 
+		String word =scan.nextLine();
+		//convert to all upper case 
+		word= word.toUpperCase();
+		//count frequency of each letter in string 
+		int i =0;
+		try {
+		for (i=0; i < word.length(); i++){ 
+			counts[word.charAt(i) - 'A']++;
+		}
+		}catch(ArrayIndexOutOfBoundsException a) {
+			System.out.println("Not a letter: " +word.charAt(i));
+			
+		}
+		//print frequencies 
+	
+		for (i=0; i<counts.length; i++) 
+			if (counts[i] != 0)
+			System.out.println((char)(i+'A')+":"+ counts[i]);
+	}
+}
---
+++ Assignments/Exception Excercise/Factorials.java
@@ -0,0 +1,22 @@
+package mahiapp;
+
+import java.util.Scanner;
+
+public class Factorials {
+	public static void main(String[] args){
+		String keepGoing = "y";
+		Scanner scan=new Scanner(System.in);
+
+		while(keepGoing.equals("y") || keepGoing.equals("Y")) {
+			try {
+			System.out.println("Enter an integer: ");
+			int val=scan.nextInt();
+			System.out.println("Factorial(" + val + ")=" + MathUtils.factorial(val));
+			System.out.println("Another factorial? (y/n) ");
+			keepGoing = scan.next();
+			}
+		catch(IllegalArgumentException i) {System.out.println(i);}
+		}
+		scan.close();
+	}
+}
